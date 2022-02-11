4d4d5187d9edd7612971db0dbac49117462bf806
Shivendra TechSter <shivendratechster@gmail.com>
2022-02-12 00:49:38 +0530

    use tailwind

--- src/App.vue
+++ src/App.vue
@@ -5,31 +5,16 @@ import Sidebar from './components/navigation/Sidebar.vue'
 </script>
 
 <template>
-  <Navbar />
-
-  <div class="content">
-    <aside>
-      <Sidebar />
-    </aside>
-    <main style="padding: 1rem;">
+  <div class="grid grid-cols-12">
+    <aside class="hidden lg:block lg:col-span-2">
+    <Sidebar />
+  </aside>
+  <main class="col-span-12 lg:col-span-10">
+    <Navbar />
+    
+    <div class="p-4">
       <RouterView />
-    </main>
+    </div>
+  </main>
   </div>
 </template>
-
-<style scoped>
-.content {
-  height: calc(100vh - 59px);
-  display: grid;
-  grid-template-columns: 215px auto;
-}
-
-aside {
-  height: 100%;
-}
-
-main {
-  min-height: 100%;
-  overflow-y: scroll;
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 
+import '@/styles/index.css'
+
 const app = createApp(App)
 
 app.use(createPinia())

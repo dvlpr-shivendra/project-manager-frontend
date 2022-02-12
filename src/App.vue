48cd526898655b1d043f6c2c5c172108cfdffc02
Shivendra TechSter <shivendratechster@gmail.com>
2022-02-12 21:06:56 +0530

    fix content view port height

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,21 @@ import Sidebar from './components/navigation/Sidebar.vue'
 <template>
   <div class="grid grid-cols-12">
     <aside class="hidden lg:block lg:col-span-2">
-    <Sidebar />
-  </aside>
-  <main class="col-span-12 lg:col-span-10">
-    <Navbar />
-    
-    <div class="p-4">
-      <RouterView />
-    </div>
-  </main>
+      <Sidebar />
+    </aside>
+    <main class="col-span-12 lg:col-span-10">
+      <Navbar />
+
+      <div class="content p-4">
+        <RouterView />
+      </div>
+    </main>
   </div>
 </template>
+
+<style scoped>
+.content {
+  height: calc(100vh - 60px);
+  overflow-y: scroll;
+}
+</style>

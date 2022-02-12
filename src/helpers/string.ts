dd2912bad3ce3f36a34daa48e9e627ce3a6c50c2
Shivendra TechSter <shivendratechster@gmail.com>
2022-02-13 00:23:22 +0530

    minor fix

--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -9,7 +9,7 @@ export default function truncate (
   text: string, length: number, suffix: string = '...'
 ) {
   if (text.length <= length)
-    return length
+    return text
 
   return text.substring(0, length) + suffix
 }

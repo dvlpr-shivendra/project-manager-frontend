972484aca00f13d038037f2c4c341d02995282ac
Shivendra TechSter <shivendratechster@gmail.com>
2022-02-16 22:01:21 +0530

    fix border issue

--- src/components/navigation/Sidebar.vue
+++ src/components/navigation/Sidebar.vue
@@ -6,38 +6,14 @@
       text-color="#ffffff"
       @open="handleOpen"
       @close="handleClose"
-      class="h-screen"
+      class="h-screen border-[#27272a]"
+      :router="true"
     >
-      <el-sub-menu index="1">
-        <template #title>
-          <el-icon>
-            <location />
-          </el-icon>
-          <span>Navigator One</span>
-        </template>
-        <el-menu-item-group title="Group One">
-          <el-menu-item index="1-1">item one</el-menu-item>
-          <el-menu-item index="1-2">item one</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="Group Two">
-          <el-menu-item index="1-3">item three</el-menu-item>
-        </el-menu-item-group>
-        <el-sub-menu index="1-4">
-          <template #title>item four</template>
-          <el-menu-item index="1-4-1">item one</el-menu-item>
-        </el-sub-menu>
-      </el-sub-menu>
-      <el-menu-item index="2">
-        <el-icon>
-          <icon-menu />
-        </el-icon>
-        <span>Navigator Two</span>
-      </el-menu-item>
-      <el-menu-item index="4">
+      <el-menu-item route="/">
         <el-icon>
           <setting />
         </el-icon>
-        <span>Navigator Four</span>
+        <span>Projects</span>
       </el-menu-item>
     </el-menu>
   </el-scrollbar>

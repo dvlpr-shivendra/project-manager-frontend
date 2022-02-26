ce2b0ae1f48ca885b7ac45ef3f3078e339e8baa6
Shivendra TechSter <shivendratechster@gmail.com>
2022-02-27 00:55:59 +0530

    change views nameing convention

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,8 +13,8 @@ const router = createRouter({
       path: '/:id',
       name: 'project',
       props: true,
-      component: () => import('../views/ProjectView.vue')
-    }
+      component: () => import('@/views/Project.vue')
+    },
   ]
 })
 

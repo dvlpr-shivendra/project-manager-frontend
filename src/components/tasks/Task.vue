c4f5187a52153e05946266203285d1e33c193b0c
Shivendra TechSter <shivendratechster@gmail.com>
2022-03-01 14:19:50 +0530

    minor fix

--- src/components/tasks/Task.vue
+++ src/components/tasks/Task.vue
@@ -1,15 +1,26 @@
 <template>
-  <input class="mb-4 w-full h-14 text-2xl px-2 hover:border rounded" v-model="task.title" placeholder="Task title"/>
+  <el-form v-model="task">
+    <input
+      class="mb-4 w-full h-14 text-2xl px-2 hover:border rounded"
+      v-model="task.title"
+      placeholder="Task title"
+    />
 
-  <el-form-item label="Deadline">
-    <el-date-picker placeholder="Click to set deadline" v-model="task.deadline" type="datetime" value-format="YYYY-MM-DD HH:mm:ss" />
-  </el-form-item>
+    <el-form-item label="Deadline">
+      <el-date-picker
+        placeholder="Click to set deadline"
+        v-model="task.deadline"
+        type="datetime"
+        value-format="YYYY-MM-DD HH:mm:ss"
+      />
+    </el-form-item>
 
-  <user-select label="Assign to" :user="task.assignee" @change="assignTo" class="w-full" />
+    <user-select label="Assign to" :user="task.assignee" @change="assignTo" class="w-full" />
 
-  <el-form-item label="Description">
-    <el-input class="mb-4" v-model="task.description" autosize type="textarea" />
-  </el-form-item>
+    <el-form-item label="Description">
+      <el-input class="mb-4" v-model="task.description" autosize type="textarea" />
+    </el-form-item>
+  </el-form>
 </template>
 
 <script lang="ts" setup>
